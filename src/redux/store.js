a79e4bedf7584026581fbe9cf48ae1dd4e3c913d
Trần Đỗ Anh Khoa <anhkhoatrando123@gmail.com>
2023-06-24 20:56:51 +0700

    I2G-41-Add play/pause overlay

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import playerReducer from "./features/playerSlice";
 import themeReducer from "./features/themeSlice";
 
 export const store = configureStore({
-    reducer: { theme: themeReducer }
+    reducer: { theme: themeReducer, player: playerReducer }
 });
