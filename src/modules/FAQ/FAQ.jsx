b6a7aaa80a02402349b8a124fc0ee7f0eed264e2
Hoang <hiiback0608@gmail.com>
2023-08-20 11:09:32 +0700

    I2G-67-25%

--- src/modules/FAQ/FAQ.jsx
+++ src/modules/FAQ/FAQ.jsx
@@ -6,9 +6,38 @@ import Layout from "../Layout";
 import { Button, QuestionAccordion, Typography } from "../../components";
 
 const FAQ = () => {
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.down("sm"));
+
+    const [page, setPage] = useState(1);
+
+    const handlePageChange = (event, value) => {
+        setPage(value);
+    };
     return (
         <Layout>
-            <div>FAQ</div>
+            <Box sx={{ backgroundColor: "#212121", color: "#fff", padding: { xs: "3rem 1rem", md: "8% 0%" } }}>
+                <Typography
+                    component="h1"
+                    size="86px"
+                    color="white"
+                    weight="extraBold"
+                    align="center"
+                    sx={{ fontFamily: "Playfair Display", lineHeight: "1.1" }}>
+                    FAQ
+                </Typography>
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        marginTop: "2.5rem"
+                    }}>
+                    <SearchBox>
+                        <StyledInputBase placeholder="Find out if we have any answered question you need" />
+                        <Search />
+                    </SearchBox>
+                </Box>
+            </Box>
         </Layout>
     );
 };
