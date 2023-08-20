b5b27ffe2a89a4bd52e18e0166df291db28ffa94
Hoang <hiiback0608@gmail.com>
2023-08-20 08:57:31 +0700

    I2G-67-init

--- src/modules/FAQ/FAQ.jsx
+++ src/modules/FAQ/FAQ.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { InputBase, styled, Box, Grid, Pagination, Tabs, useTheme, useMediaQuery } from "@mui/material";
+import { ArrowDownwardOutlined, Search } from "@mui/icons-material";
+
 import Layout from "../Layout";
+import { Button, QuestionAccordion, Typography } from "../../components";
 
 const FAQ = () => {
     return (
